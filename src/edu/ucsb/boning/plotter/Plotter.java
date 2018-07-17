5272a12ce4cb0d3510e14e50ebfdbf0935efb9b8
boningdong <boning@ucsb.edu>
2018-07-16 17:45:26 -0700

    Improved the style of plotter.

--- src/edu/ucsb/boning/plotter/Plotter.java
+++ src/edu/ucsb/boning/plotter/Plotter.java
@@ -11,13 +11,19 @@ import java.util.ArrayList;
 public class Plotter implements Runnable{
     public final static int WIDTH = 480;
     public final static int HEIGHT = 300;
+
+    // Plotter appearance style setting
+    public final static int xLabelNum = 15;
+    public final static double vPadding = 0.1;
+    public final static double scaleFactor = 1.4;
+    public final Font labelFont = new Font("Arial", Font.BOLD, 10);
     public static Plotter plotter;
 
 
     private double time = 0;
 
 
-    private int preferredTimeStampsNum = 5000;
+    private int preferredTimeStampsNum = 10000;
     private ArrayList<LogData> timeStamps = new ArrayList<>();
     private int sheepMaxNumber = 0;
     private int wolfMaxNumber = 0;
@@ -37,23 +43,19 @@ public class Plotter implements Runnable{
         frame.setLocationRelativeTo(null);
     }
 
+    public void openPlotter() {
+        if (!frame.isVisible()) {
+            frame.setVisible(true);
+        } else {
+            frame.setVisible(false);
+        }
+    }
+
     @Override
     public void run() {
         while(true) {
             render();
         }
-    }
-
-    public void render() {
-        BufferStrategy buffer = canvas.getBufferStrategy();
-        if (buffer == null) {
-            canvas.createBufferStrategy(3);
-            return;
-        }
-        Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
-        renderData(g);
-        buffer.show();
-        g.dispose();
     }
 
     public void log(double dt, int sheepNum, int wolvesNum) {
@@ -66,21 +68,51 @@ public class Plotter implements Runnable{
         wolfMaxNumber =  wolvesNum > wolfMaxNumber ? wolvesNum : wolfMaxNumber;
     }
 
-    public void renderData(Graphics2D g) {
+    public void render() {
+        BufferStrategy buffer = canvas.getBufferStrategy();
+        if (buffer == null) {
+            canvas.createBufferStrategy(3);
+            return;
+        }
+        Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.clearRect(0, 0, WIDTH, HEIGHT);
+        // Start Render
+        renderLegend(g);
+        renderLabel(g);
+        renderData(g);
+        // End Render
+        buffer.show();
+        g.dispose();
+    }
+
+    public void renderLegend(Graphics2D g) {
+        double totalTime =  timeStamps.get(timeStamps.size() - 1).getTime();
+        g.setColor(new Color(0xD5A8D6));
+        g.setFont(labelFont);
+        g.drawString(String.format("Total time: %.3f s", totalTime), (int)(WIDTH * vPadding), (int)(HEIGHT * vPadding));
+    }
+
+    public void renderLabel(Graphics2D g){
+        double dx = (double)WIDTH / timeStamps.size();
+        for (int i = 0; i < timeStamps.size() - 2; i += timeStamps.size() / xLabelNum) {
+            System.out.println(i);
+            g.setColor(new Color(0x615969));
+            g.setStroke(new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.CAP_ROUND, 0, new float[]{3}, 0));
+            g.drawLine((int)(i * dx), 5, (int)(i * dx), (int)(HEIGHT * (1-vPadding)));
+
+            g.setColor(new Color(0x887460));
+            g.setFont(labelFont);
+            g.drawString(String.format("%.2f", timeStamps.get(i).getTime()), (int)(i * dx), (int)(HEIGHT * (1 - vPadding/2)));
+        }
+    }
+
+    public void renderData(Graphics2D g) {
+
         double dx = (double)WIDTH / timeStamps.size();
         for (int i = 0; i < timeStamps.size() - 2; i++) {
             plotSheep(i, g, dx);
             plotWolves(i, g, dx);
-        }
-    }
-
-    public void openPlotter() {
-        if (!frame.isVisible()) {
-            frame.setVisible(true);
-        } else {
-            frame.setVisible(false);
         }
     }
 
@@ -105,7 +137,7 @@ public class Plotter implements Runnable{
     }
 
     private int getHeightInScale(int num, int maxNumber) {
-        double dy =  HEIGHT / (maxNumber * 1.4);
+        double dy =  HEIGHT / (maxNumber * scaleFactor);
         double height = dy * num;
         return HEIGHT - (int)height;
     }
